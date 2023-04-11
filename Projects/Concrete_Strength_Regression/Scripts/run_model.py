b935f9dde99577455aecafa0cf327477887500f5
Lee Stetson <97815072+lstetson30@users.noreply.github.com>
2023-04-11 15:33:50 -0400

    revision to run_model, transform, and tune (#1)
    * revision to run_model, transform, and tune
    
    * added classification project

--- Projects/Concrete_Strength_Regression/Scripts/run_model.py
+++ Projects/Concrete_Strength_Regression/Scripts/run_model.py
@@ -5,8 +5,8 @@ from constants import parameters
 import joblib
 import os
 
-
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
@@ -22,6 +22,7 @@ parser.add_argument('-m', '--model', type=str, help='Model to use from the follo
 parser.add_argument('--printsummary', help='Print summary statistics of the data',\
                     action='store_true')
 parser.add_argument('--testsize', type=float, help='Test size for train test split', default=0.2)
+parser.add_argument('-s', '--scale', help='Scale the data', action='store_true')
 parser.add_argument('--cv', type=int, help='Number of folds for cross validation', default=5)
 parser.add_argument('--scoring', type=str, help='Scoring metric for cross validation',\
                     default='neg_mean_squared_error')
@@ -57,16 +58,29 @@ read.saveToRawData(df)
 #Transform the data
 X_train, X_test, y_train, y_test = transform.splitData(df, 'csMPa', args.testsize)
 
+#Scale the data with standard scaler
+if args.scale:
+    X_train_unscaled, X_test_unscaled, y_train_unscaled, y_test_unscaled=\
+        X_train.copy(), X_test.copy(), y_train.copy(), y_test.copy()
+    X_train, X_test = transform.standardScaleDataframe(X_train, X_test)
+    y_train, y_test = transform.standardScaleSeries(y_train, y_test)
+
 #Tune the model to find the best Hyperparameters
 tuner = TuneTrain(model, args.cv, args.scoring)
-model_best_params, model_cv_score = tuner.optimizeHyperParams(X_train, y_train)
+tuner.optimizeHyperParams(X_train, y_train)
 
 #Train the model on the training set
 tuner.trainModel(X_train, y_train)
 
 #Evaluate the model on the test set
 test_mse = tuner.evaluateModelMSE(X_test, y_test)
-
+if args.scale:
+    eval_model = tuner.model
+    y_predicted = eval_model.predict(X_test)
+    y_predicted = transform.inverseStandardScale(y_predicted, y_train_unscaled)
+    test_mse = mean_squared_error(y_test_unscaled, y_predicted)
+    print(f'Model Test MSE Unscaled: {test_mse}')
+    
 #Train the model on the entire dataset and save it
 X, y = transform.separateFeaturesTarget(df, 'csMPa')
 tuner.trainModel(X, y)
